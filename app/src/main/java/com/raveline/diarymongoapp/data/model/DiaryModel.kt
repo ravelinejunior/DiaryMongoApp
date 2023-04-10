248033c16354972312a22ebf90f2ff7afeb9b41e
ravelinejunior <franklin.junior@ctw.bmwgroup.com>
2023-04-10 12:16:37 +0100

    25. Create a DateHeader as a Sticky Header

--- app/src/main/java/com/raveline/diarymongoapp/data/model/DiaryModel.kt
+++ app/src/main/java/com/raveline/diarymongoapp/data/model/DiaryModel.kt
@@ -1,19 +1,21 @@
 package com.raveline.diarymongoapp.data.model
 
+import com.raveline.diarymongoapp.common.utlis.toRealmInstant
 import io.realm.kotlin.ext.realmListOf
 import io.realm.kotlin.types.RealmInstant
 import io.realm.kotlin.types.RealmList
 import io.realm.kotlin.types.RealmObject
 import io.realm.kotlin.types.annotations.PrimaryKey
 import org.mongodb.kbson.ObjectId
+import java.time.Instant
 
-data class DiaryModel(
+class DiaryModel: RealmObject {
     @PrimaryKey
-    var _id: ObjectId = ObjectId.invoke(),
-    var ownerId: String = "",
-    var mood: String = Mood.Neutral.name,
-    var title: String = "",
-    var description: String = "",
-    var images: RealmList<String> = realmListOf(),
-    var date: RealmInstant = RealmInstant.from(System.currentTimeMillis(), 0)
-) : RealmObject
+    var _id: ObjectId = ObjectId.invoke()
+    var ownerId: String = ""
+    var mood: String = Mood.Neutral.name
+    var title: String = ""
+    var description: String = ""
+    var images: RealmList<String> = realmListOf()
+    var date: RealmInstant = Instant.now().toRealmInstant()
+}
